6beadbc7af5318013fc094e561996e30d34b33b2
huangluye <huang.luye@immomo.com>
2020-04-05 16:29:12 +0800

    hashtable 1

---
+++ hashtable/1.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dict = {}
+        print(dict)
+        for i, v in enumerate(nums):
+            if v in dict:
+                return [dict[v], i]
+            dict[target-v] = i
+        return []
+
+
+s = Solution()
+print(s.twoSum([2, 7, 11, 15], 9))
